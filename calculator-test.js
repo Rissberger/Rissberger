6781e4eb9d48c7b1a856b7496bcf367c8a0e4acb
Rissberger <130178293+Rissberger@users.noreply.github.com>
2023-08-11 12:04:52 -0700

    Add files via upload

---
+++ calculator-test.js
@@ -0,0 +1,14 @@
+
+it('should calculate the monthly rate correctly', function () {
+
+  const rate = {
+  }
+
+});
+
+
+it("should return a result with 2 decimal places", function() {
+  // ..
+});
+
+/// etc
